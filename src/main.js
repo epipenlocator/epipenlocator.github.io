b01a7750cf33a33f13fc1db56f15fdf4f603695f
epipenlocator <epipenlocator@gmail.com>
2025-10-19 13:05:29 +1300

    v1.1.0

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ async function init() {
     });
 }*/
 
+let manualSet = 0;
 
 document.addEventListener("DOMContentLoaded", function () {
   const mainNav = document.getElementById("mainNav");
@@ -21,13 +22,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const maps = document.getElementById("map");
   const bodyyyy = document.getElementById("body");
   const override = document.createElement("style");
+  override.setAttribute("id", "styleForStuff");
   if (!config.DISPLAY_NAVBAR) {
     mainNav.style.height = "100%";
     navbadr.style.display = "none";
   } else {
     navbadr.style.display = "flex";
-    bodyyyy.style.height = "calc(100% - 64px)";
-    maps.style.height = "calc(100% - 69px) !important";
+    bodyyyy.style.height = "calc(100% - 128px)";
+    /*maps.style.height = "calc(100% - 69px) !important";*/
     override.innerText = "@media screen and (min-width:640px) {#map {height: calc(100% - 62px) !important;top:62px !important;}}"
     document.head.appendChild(override);
   }
@@ -67,6 +69,7 @@ var data = [];
 function showTab(tabId, element) {
   document.getElementById('map').style.display = 'none';
   document.getElementById('settings_tab').style.display = 'none';
+  document.getElementById('findbox').style.display = 'none';
 
   document.getElementById(tabId).style.display = 'block';
   const tabs = document.querySelectorAll('.tab-selector');
@@ -136,8 +139,7 @@ async function getLocationFromIp() {
   const returndata = { "isp": ip2.isp, "city": ip2.city, "country": ip2.country, "latitude": ip2.latitude, "longitude": ip2.longitude };
   return returndata;
 }
-async function setLocationOnMap() {
-
+async function setLocationOnMap(latitude_marker = 0, longitude_marker = 0) {
   //console.log(info);
   var lat;
   var lng;
@@ -165,88 +167,108 @@ async function setLocationOnMap() {
     //console.log(locationPopup);
     return locationPopup;
   }
+  if (manualSet == 0) {
+    if (navigator.geolocation && navigator.userAgent.search("Firefox") == -1) {
+      async function showPosition(position) {
+        function hideErr() {
+          const errorbox = document.getElementById("errorbox");
+          const body = document.getElementById("main");
+          errorbox.style.display = "none";
+          body.style.display = "block";
+        }
+        if (position) {
+          lat = position.coords.latitude;
+          lng = position.coords.longitude;
+          myLocation.setLatLng([lat, lng]);
+          myLocation.bringToBack();
+          if (firstTime == 1) {
+            map.setView([position.coords.latitude, position.coords.longitude], 15);
+            firstTime = 0;
+          } else {
+            console.log("NO UPDATE");
+          }
+          await findNearestLocation(position.coords.latitude, position.coords.longitude);
+          var info = await getLocationFromIp();
+          myLocation.bindPopup(doPopup(info));
+          //myLocation.bindPopup(locationPopup);
+          hideErr();
 
-  if (navigator.geolocation && navigator.userAgent.search("Firefox") == -1) {
-    async function showPosition(position) {
-      function hideErr() {
-        const errorbox = document.getElementById("errorbox");
-        const body = document.getElementById("main");
-        errorbox.style.display = "none";
-        body.style.display = "block";
+        } else {
+          console.log("N/A");
+        }
       }
-      if (position) {
-        lat = position.coords.latitude;
-        lng = position.coords.longitude;
+      function errorOut(error) {
+        function showError(err) {
+          if (err != "Location information is unavailable.") {
+            const body = document.getElementById("main");
+            const errorbox = document.getElementById("errorbox");
+            const materialbottom = document.getElementById("material-bottom");
+            body.style.display = "none";
+            materialbottom.style.display = "none";
+            errorbox.style.display = "block";
+
+            errorbox.innerHTML = `<h3>Error</h3><br>${err}`;
+          }
+        }
+        switch (error.code) {
+          case error.PERMISSION_DENIED:
+            /*showError("Location permission has not been granted.\
+                        Please grant this permission.");*/
+            showError("Epipen Locator requires your location permission to be turned <b>on</b>.");
+            break;
+          case error.POSITION_UNAVAILABLE:
+            showError("Location information is unavailable.");
+            break;
+          case error.TIMEOUT:
+            showError("The request to get user location timed out.");
+            break;
+          case error.UNKNOWN_ERROR:
+            showError("An unknown error occurred.");
+            break;
+        }
+      }
+      navigator.geolocation.getCurrentPosition(showPosition, errorOut);
+    } else {
+      var info = await getLocationFromIp();
+      if (info) {
+        //myLocation = L.marker([info.latitude, info.longitude]);
+        lat = info.latitude;
+        lng = info.longitude;
         myLocation.setLatLng([lat, lng]);
-        myLocation.bringToBack();
+        //myLocation.bindPopup(locationPopup);
         if (firstTime == 1) {
-          map.setView([position.coords.latitude, position.coords.longitude], 15);
+          map.setView([info.latitude, info.longitude], 15);
           firstTime = 0;
         } else {
           console.log("NO UPDATE");
         }
-        await findNearestLocation(position.coords.latitude, position.coords.longitude);
-        var info = await getLocationFromIp();
         myLocation.bindPopup(doPopup(info));
-        //myLocation.bindPopup(locationPopup);
-        hideErr();
-
-      } else {
-        console.log("N/A");
+        myLocation.bringToBack();
+        await findNearestLocation(info.latitude, info.longitude);
       }
     }
-    function errorOut(error) {
-      function showError(err) {
-        if (err != "Location information is unavailable.") {
-          const body = document.getElementById("main");
-          const errorbox = document.getElementById("errorbox");
-          const materialbottom = document.getElementById("material-bottom");
-          body.style.display = "none";
-          materialbottom.style.display = "none";
-          errorbox.style.display = "block";
-
-          errorbox.innerHTML = `<h3>Error</h3><br>${err}`;
-        }
-      }
-      switch (error.code) {
-        case error.PERMISSION_DENIED:
-          /*showError("Location permission has not been granted.\
-                      Please grant this permission.");*/
-          showError("Epipen Locator requires your location permission to be turned <b>on</b>.");
-          break;
-        case error.POSITION_UNAVAILABLE:
-          showError("Location information is unavailable.");
-          break;
-        case error.TIMEOUT:
-          showError("The request to get user location timed out.");
-          break;
-        case error.UNKNOWN_ERROR:
-          showError("An unknown error occurred.");
-          break;
-      }
-    }
-    navigator.geolocation.getCurrentPosition(showPosition, errorOut);
+    latitude = lat;
+    longitude = lng;
   } else {
     var info = await getLocationFromIp();
-    if (info) {
-      //myLocation = L.marker([info.latitude, info.longitude]);
-      lat = info.latitude;
-      lng = info.longitude;
-      myLocation.setLatLng([lat, lng]);
-      //myLocation.bindPopup(locationPopup);
-      if (firstTime == 1) {
-        map.setView([info.latitude, info.longitude], 15);
-        firstTime = 0;
-      } else {
-        console.log("NO UPDATE");
-      }
-      myLocation.bindPopup(doPopup(info));
-      myLocation.bringToBack();
-      await findNearestLocation(info.latitude, info.longitude);
+    lat = latitude_marker;
+    lng = longitude_marker;
+    myLocation.setLatLng([lat, lng]);
+    //myLocation.bindPopup(locationPopup);
+    if (firstTime == 1) {
+      map.setView([latitude_marker, longitude_marker], 15);
+      firstTime = 0;
+    } else {
+      console.log("NO UPDATE");
     }
+    myLocation.bindPopup(doPopup(info));
+    myLocation.bindTooltip("<b>Selected location</b>", {
+      permanent: true,   // stays visible, doesn't require hover
+      direction: "top" // position relative to the marker
+    });
+    myLocation.bringToBack();
+    await findNearestLocation(latitude_marker, longitude_marker);
   }
-  latitude = lat;
-  longitude = lng;
 }
 
 function doGoofyShitForSamsung() {
@@ -387,6 +409,28 @@ function showDetails() {
     box.open = false;
     text.innerText = "Open";
   }
+}
+
+async function flyTo(city, postcode, country) {
+  let data = await fetch(`https://corsproxy.io/?url=https://nominatim.openstreetmap.org/search?city=${city}&postalcode=${postcode}&country=${country}&format=jsonv2`);
+  let jsondata = await data.json();
+  console.log(jsondata[0]);
+  manualSet = 1;
+  firstTime = 1;
+  setLocationOnMap(jsondata[0]["lat"], jsondata[0]["lon"]);
+
+  const mapicon = document.getElementById("mapicon");
+  mapicon.click();
+}
+
+function fixHead() {
+  /*const stylestuff = document.getElementById("styleForStuff");
+  if (stylestuff) {
+    stylestuff.innerText = "@media screen and (min-width:640px) {#map {height: 100% !important;top:0px !important;}}";
+  } else {
+    console.warn('Style element with id "styleForStuff" not found.');
+  }*/
+  return 1;
 }
 
 window.showKey = showKey;
