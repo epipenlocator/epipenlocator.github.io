08121f66db99ba729c4c193d430322b6f374268c
epipenlocator <epipenlocator@gmail.com>
2025-10-19 18:19:05 +1300

    v1.1.1

--- src/main.js
+++ src/main.js
@@ -64,7 +64,7 @@ html: `<span style="${markerHtmlStyles}" />`
 })*/
 let latitude = 0;
 let longitude = 0;
-var firstTime = 1;
+let firstTime = 1;
 var data = [];
 function showTab(tabId, element) {
   document.getElementById('map').style.display = 'none';
@@ -77,7 +77,7 @@ function showTab(tabId, element) {
 
   element.classList.add('active');
 }
-var map = L.map('map').setView([-40.900557, 174.885971], 6);
+let map = L.map('map').setView([-40.900557, 174.885971], 6);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> | Data &copy; Epipen Locator'
@@ -91,9 +91,8 @@ async function getData() {
   return data;
 }
 async function doMap() {
-  await setLocationOnMap();
+  await setLocationOnMap(null, null, 1);
   let data = await getData();
-  console.log(data);
   data.forEach(element => {
     var marker = L.marker([element[0], element[1]]).addTo(map);
     if (element[11] == "Purchase") {
@@ -133,143 +132,112 @@ async function doMap() {
 async function getLocationFromIp() {
   let myip = await fetch(`https://api.ip.sb/geoip/`);
   const ip2 = await myip.json();
-  console.log(ip2)
   //let ip = await fetch(`https://ip-api.io/api/v1/ip/${ip2.ip}`);
   //const data = await ip.json();
   const returndata = { "isp": ip2.isp, "city": ip2.city, "country": ip2.country, "latitude": ip2.latitude, "longitude": ip2.longitude };
   return returndata;
 }
-async function setLocationOnMap(latitude_marker = 0, longitude_marker = 0) {
-  //console.log(info);
-  var lat;
-  var lng;
-  function doPopup(info) {
-    var locationPopup = `<b>Your Location</b>`
 
-    //if (info.city != "null") {
-    //  var locationPopup = `${locationPopup}<br>${info.city}, ${info.country}`;
-    //} else {
-    var locationPopup = `${locationPopup}<br>${info.country}`;
-    //}
-    //var
+/* START REWRITE 19-10-2025 */
+let selected_lat; let selected_long;
+let function_lat; let function_long;
 
-    var locationPopup = `${locationPopup}<br><br>`;
+function doPopup(info) {
+  var locationPopup = `<b>Your Location</b>`
+  var locationPopup = `${locationPopup}<br>${info.country}`;
+  var locationPopup = `${locationPopup}<br><br>`;
 
-    if (info.isp == "Spark New Zealand") {
-      locationPopup = `${locationPopup}On <b style='color:rebeccapurple;'>Spark NZ</b>`;
-    } else if (info.isp == "One NZ") {
-      locationPopup = `${locationPopup}On <b style='color:green;'>One NZ</b>`;
-    } else if (info.isp == "2degrees Mobile" || info.isp == "2degrees") {
-      locationPopup = `${locationPopup}On <b style='color:dodgerblue;'>2degrees</b>`;
-    } else {
-      locationPopup = `${locationPopup}On <b>${info.isp}</b>`;
-    }
-    //console.log(locationPopup);
-    return locationPopup;
-  }
-  if (manualSet == 0) {
-    if (navigator.geolocation && navigator.userAgent.search("Firefox") == -1) {
-      async function showPosition(position) {
-        function hideErr() {
-          const errorbox = document.getElementById("errorbox");
-          const body = document.getElementById("main");
-          errorbox.style.display = "none";
-          body.style.display = "block";
-        }
-        if (position) {
-          lat = position.coords.latitude;
-          lng = position.coords.longitude;
-          myLocation.setLatLng([lat, lng]);
-          myLocation.bringToBack();
-          if (firstTime == 1) {
-            map.setView([position.coords.latitude, position.coords.longitude], 15);
-            firstTime = 0;
-          } else {
-            console.log("NO UPDATE");
-          }
-          await findNearestLocation(position.coords.latitude, position.coords.longitude);
-          var info = await getLocationFromIp();
-          myLocation.bindPopup(doPopup(info));
-          //myLocation.bindPopup(locationPopup);
-          hideErr();
+  if (info.isp == "Spark New Zealand") { locationPopup = `${locationPopup}On <b style='color:rebeccapurple;'>Spark NZ</b>`; }
+  else if (info.isp == "One NZ") { locationPopup = `${locationPopup}On <b style='color:green;'>One NZ</b>`; }
+  else if (info.isp == "2degrees Mobile" || info.isp == "2degrees") { locationPopup = `${locationPopup}On <b style='color:dodgerblue;'>2degrees</b>`; }
+  else { locationPopup = `${locationPopup}On <b>${info.isp}</b>`; }
+  return locationPopup;
+}
+function showError(err) {
+  if (err != "Location information is unavailable.") {
+    const body = document.getElementById("main");
+    const errorbox = document.getElementById("errorbox");
+    const materialbottom = document.getElementById("material-bottom");
+    body.style.display = "none";
+    materialbottom.style.display = "none";
+    errorbox.style.display = "block";
 
-        } else {
-          console.log("N/A");
-        }
-      }
-      function errorOut(error) {
-        function showError(err) {
-          if (err != "Location information is unavailable.") {
-            const body = document.getElementById("main");
-            const errorbox = document.getElementById("errorbox");
-            const materialbottom = document.getElementById("material-bottom");
-            body.style.display = "none";
-            materialbottom.style.display = "none";
-            errorbox.style.display = "block";
-
-            errorbox.innerHTML = `<h3>Error</h3><br>${err}`;
-          }
-        }
-        switch (error.code) {
-          case error.PERMISSION_DENIED:
-            /*showError("Location permission has not been granted.\
-                        Please grant this permission.");*/
-            showError("Epipen Locator requires your location permission to be turned <b>on</b>.");
-            break;
-          case error.POSITION_UNAVAILABLE:
-            showError("Location information is unavailable.");
-            break;
-          case error.TIMEOUT:
-            showError("The request to get user location timed out.");
-            break;
-          case error.UNKNOWN_ERROR:
-            showError("An unknown error occurred.");
-            break;
-        }
-      }
-      navigator.geolocation.getCurrentPosition(showPosition, errorOut);
-    } else {
-      var info = await getLocationFromIp();
-      if (info) {
-        //myLocation = L.marker([info.latitude, info.longitude]);
-        lat = info.latitude;
-        lng = info.longitude;
-        myLocation.setLatLng([lat, lng]);
-        //myLocation.bindPopup(locationPopup);
-        if (firstTime == 1) {
-          map.setView([info.latitude, info.longitude], 15);
-          firstTime = 0;
-        } else {
-          console.log("NO UPDATE");
-        }
-        myLocation.bindPopup(doPopup(info));
-        myLocation.bringToBack();
-        await findNearestLocation(info.latitude, info.longitude);
-      }
-    }
-    latitude = lat;
-    longitude = lng;
-  } else {
-    var info = await getLocationFromIp();
-    lat = latitude_marker;
-    lng = longitude_marker;
-    myLocation.setLatLng([lat, lng]);
-    //myLocation.bindPopup(locationPopup);
-    if (firstTime == 1) {
-      map.setView([latitude_marker, longitude_marker], 15);
-      firstTime = 0;
-    } else {
-      console.log("NO UPDATE");
-    }
-    myLocation.bindPopup(doPopup(info));
-    myLocation.bindTooltip("<b>Selected location</b>", {
-      permanent: true,   // stays visible, doesn't require hover
-      direction: "top" // position relative to the marker
-    });
-    myLocation.bringToBack();
-    await findNearestLocation(latitude_marker, longitude_marker);
+    errorbox.innerHTML = `<h3>Error</h3><br>${err}`;
   }
 }
+function errorOut(error) {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      showError("Epipen Locator requires your location permission to be turned <b>on</b>.");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      showError("Location information is unavailable.");
+      break;
+    case error.TIMEOUT:
+      showError("The request to get user location timed out.");
+      break;
+    case error.UNKNOWN_ERROR:
+      showError("An unknown error occurred.");
+      break;
+  }
+}
+function hideErr() {
+  const errorbox = document.getElementById("errorbox");
+  const body = document.getElementById("main");
+  errorbox.style.display = "none";
+  body.style.display = "block";
+}
+async function showPosition(position) {
+  if (position) {
+    selected_lat = await position.coords.latitude;
+    selected_long = await position.coords.longitude;
+  }
+}
+
+async function setLocationOnMap(latitude_marker = null, longitude_marker = null, isFirstTime = 0) {
+  let label = "You are here";
+  var info = await getLocationFromIp();
+
+  if (manualSet == 0) {
+    if (navigator.geolocation && navigator.userAgent.search("Firefox") == -1) {
+      navigator.geolocation.getCurrentPosition(showPosition, errorOut);
+    } else {
+      if (info) {
+        selected_lat = await info.latitude;
+        selected_long = await info.longitude;
+      }
+    }
+  } else {
+    if (latitude_marker != null || longitude_marker != null) {
+      selected_lat = latitude_marker; function_lat = latitude_marker;
+      selected_long = longitude_marker; function_long = longitude_marker;
+    } else {
+      selected_lat = function_lat;
+      selected_long = function_long;
+    }
+    label = "Selected location"
+  }
+
+  if (selected_lat == undefined || selected_long == undefined) {
+    if (info) {
+      selected_lat = await info.latitude;
+      selected_long = await info.longitude;
+    }
+  }
+
+  myLocation.setLatLng([selected_lat, selected_long]);
+  myLocation.bringToBack();
+  myLocation.bindTooltip(`<b>${label}</b>`, { permanent: true, direction: "top" });
+  myLocation.bindPopup(doPopup(info));
+  if (isFirstTime == 1) {
+    map.setView([selected_lat, selected_long], 15);
+  }
+  hideErr();
+
+  await findNearestLocation(selected_lat, selected_long, isFirstTime);
+  /*firstTime = 0;*/
+}
+/* END REWRITE 19-10-2025 */
 
 function doGoofyShitForSamsung() {
   let userAgentString = navigator.userAgent;
@@ -343,14 +311,11 @@ async function getNearestCoordinate(userLat, userLng, data, firstTime) {
   let minDistance = Infinity;
 
   for (const item of data) {
-    console.log(item);
     let lat2 = parseFloat(item[0]);
     let lng2 = parseFloat(item[1]);
-    console.log(lat2, lng2);
 
     userLat = parseFloat(userLat);
     userLng = parseFloat(userLng);
-    console.log(userLat, userLng);
 
     let distance = Math.sqrt(Math.pow(lat2 - userLat, 2) + Math.pow(lng2 - userLng, 2));
     //console.log(`Distance to (${lat2}, ${lng2}):`, distance);
@@ -364,12 +329,12 @@ async function getNearestCoordinate(userLat, userLng, data, firstTime) {
   const box = document.getElementById("nearest");
   box.innerHTML = `<summary><table id="tableNav"><tr><td><b>Nearest Location</b><td><button onclick="showDetails();">Close</button></td></tr></table></summary><div id="content"><p><b>${nearest[2]}</b></p><br><p>${nearest[3]}, <br>${nearest[4]}, <br>${nearest[5]}</p></div>`;
   // Samsung Internet is weird so we need this stuff
-  if (firstTime == true) {
+  if (firstTime == 1) {
     box.open = true;
   }
 }
 
-async function findNearestLocation(userLat, userLng, firstTime = true) {
+async function findNearestLocation(userLat, userLng, firstTime = 0) {
   const data = await getData();
   await getNearestCoordinate(userLat, userLng, data, firstTime);
   //const box = document.getElementById("nearest");
@@ -412,12 +377,16 @@ function showDetails() {
 }
 
 async function flyTo(city, postcode, country) {
-  let data = await fetch(`https://corsproxy.io/?url=https://nominatim.openstreetmap.org/search?city=${city}&postalcode=${postcode}&country=${country}&format=jsonv2`);
-  let jsondata = await data.json();
-  console.log(jsondata[0]);
-  manualSet = 1;
-  firstTime = 1;
-  setLocationOnMap(jsondata[0]["lat"], jsondata[0]["lon"]);
+  if (city != "Current") {
+    let data = await fetch(`https://corsproxy.io/?url=https://nominatim.openstreetmap.org/search?city=${city}&postalcode=${postcode}&country=${country}&format=jsonv2`);
+    let jsondata = await data.json();
+    console.log(jsondata[0]);
+    manualSet = 1;
+    setLocationOnMap(jsondata[0]["lat"], jsondata[0]["lon"], 1);
+  } else {
+    manualSet = 0;
+    setLocationOnMap(null, null, 1);
+  }
 
   const mapicon = document.getElementById("mapicon");
   mapicon.click();
